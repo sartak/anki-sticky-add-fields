8f37f8f5cf323a9dfeb49a0bfa160f4f5ca0b910
Shawn M Moore <sartak@gmail.com>
2010-02-15 14:18:34 -0500

    Most of the rest of the implementation

--- Sticky Add Fields.py
+++ Sticky Add Fields.py
@@ -19,19 +19,30 @@ from anki.hooks import wrap
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
+# for AddCards:
 def initializeNewFact(self, old_fact, **kw):
     f = kw['_old'](self, old_fact)
-    #f[u'起こり'] = old_fact[u'起こり']
+
+    for field_name, checkbox in self.editor.checkboxes.items():
+        if checkbox.isChecked():
+            f[field_name] = old_fact[field_name]
+
     return f
+
+# for FactEditor:
+def addCheckBoxAttr(self, field, n):
+    self.checkboxes = {}
 
 def addCheckBox(self, field, n):
     if self.addMode:
         c = QCheckBox()
         c.setToolTip(_("Sticky: keep this value between successive Adds"))
+        self.checkboxes[field.name] = c
         self.fieldsGrid.addWidget(c, n, 2)
 
-#Setup our hook
+#Setup our hooks
 if not __name__ == "__main__":
     AddCards.initializeNewFact = wrap(AddCards.initializeNewFact, initializeNewFact, "around")
+    FactEditor.drawFields = wrap(FactEditor.drawFields, addCheckBoxAttr, "before")
     FactEditor.drawField = wrap(FactEditor.drawField, addCheckBox, "after")
 
