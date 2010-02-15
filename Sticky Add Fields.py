0a9bf8cff96dacb1568dbf9716cf88834fe4b85f
Shawn M Moore <sartak@gmail.com>
2010-02-15 13:58:46 -0500

    Add checkboxes to the fact manager

--- Sticky Add Fields.py
+++ Sticky Add Fields.py
@@ -14,6 +14,7 @@ Shawn M Moore (sartak@gmail.com)
 """
 
 from ankiqt.ui.addcards import AddCards
+from ankiqt.ui.facteditor import FactEditor
 from anki.hooks import wrap
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
@@ -23,7 +24,14 @@ def initializeNewFact(self, old_fact, **kw):
     #f[u'起こり'] = old_fact[u'起こり']
     return f
 
+def addCheckBox(self, field, n):
+    if self.addMode:
+        c = QCheckBox()
+        c.setToolTip(_("Sticky: keep this value between successive Adds"))
+        self.fieldsGrid.addWidget(c, n, 2)
+
 #Setup our hook
 if not __name__ == "__main__":
     AddCards.initializeNewFact = wrap(AddCards.initializeNewFact, initializeNewFact, "around")
+    FactEditor.drawField = wrap(FactEditor.drawField, addCheckBox, "after")
 
